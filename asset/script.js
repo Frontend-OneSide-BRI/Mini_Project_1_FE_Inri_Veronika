49b6a8632ef072ad9e2e754f83e34803a157b283
Inri692 <inriveronika17@gmail.com>
2023-05-31 23:19:00 +0700

    add gallery page

--- asset/script.js
+++ asset/script.js
@@ -1,59 +1,111 @@
-const images = [
-  {
-    name: "Gambar 1",
-    type: "olahraga",
-    url: "https://www.bcalife.co.id/storage/articles/share/bagus-untuk-kesehatan-simak-5-manfaat-olahraga-bagi-tubuh-ini-pantang-diabaikan-1653392364.jpg",
-  },
-  {
-    name: "Gambar 2",
-    type: "hiburan",
-    url: "https://primaberita.com/wp-content/uploads/2020/11/Suroboyo-Carnival-Park-1.jpe",
-  },
-  {
-    name: "Gambar 3",
-    type: "seni",
-    url: "https://www.blibli.com/friends-backend/wp-content/uploads/2023/01/B100114-cover-scaled.jpg",
-  },
-  {
-    name: "Gambar 4",
-    type: "sosial",
-    url: "https://cdn1-production-images-kly.akamaized.net/cSkMxnr0kzgGWbtHFzAspshP58A=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3390703/original/065507600_1614673949-akson-1K8pIbIrhkQ-unsplash__1_.jpg",
-  },
-  {
-    name: "Gambar 5",
-    type: "olahraga",
-    url: "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/06/15012412/kesehatan-olahraga.jpg",
-  },
-  {
-    name: "Gambar 6",
-    type: "seni",
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfhYv9qcHMVKeKIIz8eIL_VQ7CJp9RTK4juEpWbPOWUVhU-urT_kjUdOna9H-avehVQXo&usqp=CAU",
-  },
-  {
-    name: "Gambar 7",
-    type: "hiburan",
-    url: "https://blog.klikindomaret.com/wp-content/uploads/2021/01/carousel-horses-carnival-merry-go-round_1-1.jpg",
-  },
-];
+let products = {
+  data: [
+    {
+      productName: "Regular White T-Shirt",
+      category: "Topwear",
+      image: "./image/white-tshirt.jpg",
+    },
+    {
+      productName: "Beige Short Skirt",
+      category: "Bottomwear",
+      image: "./image/short-skirt.jpg",
+    },
+    {
+      productName: "Sporty SmartWatch",
+      category: "Watch",
+      image: "./image/sporty-smartwatch.jpg",
+    },
+    {
+      productName: "Basic Knitted Top",
+      category: "Topwear",
+      image: "./image/knitted-top.jpg",
+    },
+    {
+      productName: "Black Leather Jacket",
+      category: "Jacket",
+      image: "./image/black-leather-jacket.jpg",
+    },
+    {
+      productName: "Stylish Pink Trousers",
+      category: "Bottomwear",
+      image: "./image/pink-trousers.jpg",
+    },
+    {
+      productName: "Brown Men's Jacket",
+      category: "Jacket",
+      image: "./image/brown-jacket.jpg",
+    },
+    {
+      productName: "Comfy Gray Pants",
+      category: "Bottomwear",
+      image: "./image/comfy-gray-pants.jpg",
+    },
+  ],
+};
 
-$("#recipeCarousel").carousel({
-  interval: 10000,
-});
+for (let i of products.data) {
+  let card = document.createElement("div");
+  card.classList.add("card", i.category, "hide");
 
-$(".carousel .carousel-item").each(function () {
-  var minPerSlide = 3;
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(":first");
-  }
-  next.children(":first-child").clone().appendTo($(this));
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
 
-  for (var i = 0; i < minPerSlide; i++) {
-    next = next.next();
-    if (!next.length) {
-      next = $(this).siblings(":first");
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+
+  let container = document.createElement("div");
+  container.classList.add("container");
+
+  let name = document.createElement("h5");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+function filterProduct(value) {
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
     }
+  });
 
-    next.children(":first-child").clone().appendTo($(this));
-  }
+  let elements = document.querySelectorAll(".card");
+
+  elements.forEach((element) => {
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      if (element.classList.contains(value)) {
+        element.classList.remove("hide");
+      } else {
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+document.getElementById("search").addEventListener("click", () => {
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  elements.forEach((element, index) => {
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      cards[index].classList.remove("hide");
+    } else {
+      cards[index].classList.add("hide");
+    }
+  });
 });
+
+window.onload = () => {
+  filterProduct("all");
+};
